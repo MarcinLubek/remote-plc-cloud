35bf108f24bf4aa66b09cbb6a17a691f0cc380f8
Marcin Łubek <m.lubek@polsource.com>
2019-07-08 13:25:30 +0200

    socket test

--- socketServer.js
+++ socketServer.js
@@ -1,24 +1,24 @@
-var ioCloud = require('socket.io')();
+// var ioCloud = require('socket.io')();
 var ioLocal = require('socket.io')();
 
-ioCloud.on('connection', (client) => {
-    console.log('connected');
-    client.on('up', () => {
-        ioCloud.sockets.emit('up');
-        console.log('socketServer up!');
-    })
-    client.on('down', () => {
-        ioCloud.sockets.emit('down');
-        console.log('socketServer down!');
-    })
-    client.on('stop', () => {
-        ioCloud.sockets.emit('stop');
-        console.log('socketServer stop!');
-    })
-});
+// ioCloud.on('connection', (client) => {
+//     console.log('cloud connected');
+//     client.on('up', () => {
+//         ioCloud.sockets.emit('up');
+//         console.log('socketServer up!');
+//     })
+//     client.on('down', () => {
+//         ioCloud.sockets.emit('down');
+//         console.log('socketServer down!');
+//     })
+//     client.on('stop', () => {
+//         ioCloud.sockets.emit('stop');
+//         console.log('socketServer stop!');
+//     })
+// });
 
 ioLocal.on('connection', (client) => {
-    console.log('connected');
+    console.log('local connected');
     client.on('up', () => {
         // ioCloud.sockets.emit('up');
         // console.log('socketServer up!');
@@ -33,11 +33,11 @@ ioLocal.on('connection', (client) => {
     })
 });
 
-const portCloud = 3001;
+// const portCloud = 3001;
 const portLocal = 3002;
 
-ioCloud.listen(portCloud);
-console.log('cloud listening on port', portCloud);
+// ioCloud.listen(portCloud);
+// console.log('cloud listening on port', portCloud);
 
 ioLocal.listen(portLocal);
 console.log('local listening on port', portLocal);
