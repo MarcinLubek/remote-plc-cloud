a81261bf95a87124eac08e8f6185bac1f18df1f9
Marcin Łubek <m.lubek@polsource.com>
2019-07-14 18:26:42 +0200

    faza

--- socketServer.js
+++ socketServer.js
@@ -1,65 +1,85 @@
-//
-// Local
-//
+let localClient = require("socket.io")();
+let cloudClient = require("socket.io")();
 
-let ioLocal = require("socket.io")();
+localClient.listen(3001);
+cloudClient.listen(3002);
 
-let actuatorData;
+let serverData;
 
-ioLocal.on("connection", client => {
-  console.log("local connected");
-  client.on("actuatorData", () => {
-    console.log("faza");
-  });
-  client.on("disconnect", () => {
-    console.log("local disconnected");
-  });
-  // client.on('up', () => {
-  //     console.log('socketServer ioLocal up');
-  //     // ioCloud.sockets.emit('up');
-  //     // console.log('socketServer up!');
-  // })
-  // client.on('down', () => {
-  //     console.log('socketServer ioLocal down');
-  //     // ioCloud.sockets.emit('down');
-  //     // console.log('socketServer down!');
-  // })
-  // client.on('stop', () => {
-  //     console.log('socketServer ioLocal stop');
-  //     // ioCloud.sockets.emit('stop');
-  //     // console.log('socketServer stop!');
-  // })
+localClient.on("connect", client => {
+	console.log("local connected");
+	client.on("data", data => {
+		serverData = data;
+	});
 });
-const portLocal = 3001;
 
-ioLocal.listen(portLocal);
-console.log("local listening on port", portLocal);
-
-//
-// Cloud
-//
-
-let ioCloud = require("socket.io")();
-
-ioCloud.on("connection", client => {
-  console.log("cloud connected");
-  client.on("up", () => {
-    ioLocal.sockets.emit("up");
-    console.log("cloud emits up!");
-  });
-  client.on("down", () => {
-    ioLocal.sockets.emit("down");
-    console.log("cloud emits down!");
-  });
-  client.on("stop", () => {
-    ioLocal.sockets.emit("stop");
-    console.log("cloud emits stop!");
-  });
-  client.on("disconnect", () => {
-    console.log("cloud disconnected");
-  });
+cloudClient.on("connect", client => {
+	console.log("cloud connected");
+	client.emit("data", serverData);
 });
-const portCloud = 3002;
 
-ioCloud.listen(portCloud);
-console.log("cloud listening on port", portCloud);
+// //
+// // Local
+// //
+
+// let ioLocal = require("socket.io")();
+
+// let actuatorData;
+
+// ioLocal.on("connection", client => {
+//   console.log("local connected");
+//   client.on("actuatorData", () => {
+//     console.log("faza");
+//   });
+//   client.on("disconnect", () => {
+//     console.log("local disconnected");
+//   });
+//   // client.on('up', () => {
+//   //     console.log('socketServer ioLocal up');
+//   //     // ioCloud.sockets.emit('up');
+//   //     // console.log('socketServer up!');
+//   // })
+//   // client.on('down', () => {
+//   //     console.log('socketServer ioLocal down');
+//   //     // ioCloud.sockets.emit('down');
+//   //     // console.log('socketServer down!');
+//   // })
+//   // client.on('stop', () => {
+//   //     console.log('socketServer ioLocal stop');
+//   //     // ioCloud.sockets.emit('stop');
+//   //     // console.log('socketServer stop!');
+//   // })
+// });
+// const portLocal = 3001;
+
+// ioLocal.listen(portLocal);
+// console.log("local listening on port", portLocal);
+
+// //
+// // Cloud
+// //
+
+// let ioCloud = require("socket.io")();
+
+// ioCloud.on("connection", client => {
+//   console.log("cloud connected");
+//   client.on("up", () => {
+//     ioLocal.sockets.emit("up");
+//     console.log("cloud emits up!");
+//   });
+//   client.on("down", () => {
+//     ioLocal.sockets.emit("down");
+//     console.log("cloud emits down!");
+//   });
+//   client.on("stop", () => {
+//     ioLocal.sockets.emit("stop");
+//     console.log("cloud emits stop!");
+//   });
+//   client.on("disconnect", () => {
+//     console.log("cloud disconnected");
+//   });
+// });
+// const portCloud = 3002;
+
+// ioCloud.listen(portCloud);
+// console.log("cloud listening on port", portCloud);
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,102 +1,114 @@
-import React, { Component } from 'react';
-import Menu from './Menu';
-import Control from './Control';
-import ConnectionStatus from './ConnectionStatus';
-import 'material-design-icons/iconfont/material-icons.css';
+import React, { Component } from "react";
+import Menu from "./Menu";
+import Control from "./Control";
+import ConnectionStatus from "./ConnectionStatus";
+import "material-design-icons/iconfont/material-icons.css";
 
-import openSocket from 'socket.io-client';
+import openSocket from "socket.io-client";
 
 class Dashboard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			connected: false,
+			socket: null
+		};
+		this.connect = this.connect.bind(this);
+		this.disconnect = this.disconnect.bind(this);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            connected: false,
-            socket: null
-        }
-        this.connect = this.connect.bind(this);
-        this.disconnect = this.disconnect.bind(this);
-    }
+	connect() {
+		this.props.loading();
+		let socket = openSocket("http://vps699582.ovh.net:3002");
 
-    connect() {
-        this.props.loading();
-        let socket = openSocket('http://vps699582.ovh.net:3003');
-        socket.io.on('connect_error', function (err) {
-            // handle server error here
-            console.log('Error connecting to server');
-        });
+		socket.on("connect", () => {
+			console.log("client 2 on connect");
+		});
 
-        this.setState({
-            connected: true,
-            socket: socket
-        });
-        this.props.loadingStop();
-    }
+		socket.on("data", data => {
+			console.log("client 2 on data", data);
+		});
 
-    disconnect() {
-        this.props.loading();
-        this.setState({
-            connected: false,
-            socket: null
-        });
-        this.props.loadingStop();
-    }
+		socket.io.on("connect_error", function (err) {
+			// handle server error here
+			console.log("Error connecting to server");
+		});
 
-    render() {
-        if (this.props.isAuthorized) {
-            if (this.state.connected) {
-                return (
-                    <div className="container">
-                        <div className="bufferContainer">
-                            <ConnectionStatus
-                                connectionState={this.state.connected}
-                                connect={this.connect}
-                                disconnect={this.disconnect} />
-                            <div id="menuContainerId" className="menuContainer">
-                                <Menu
-                                    navigateToSettings={this.props.navigateToSettings}
-                                    navigateToDashboard={this.props.navigateToDashboard} />
-                            </div>
-                        </div>
-                        <Control socket={this.state.socket} />
-                    </div >
-                );
-            } else {
-                return (
-                    <div className="container">
-                        <div className="bufferContainer">
-                            <ConnectionStatus
-                                connectionState={this.state.connected}
-                                connect={this.connect}
-                                disconnect={this.disconnect} />
-                            <div id="menuContainerId" className="menuContainer">
-                                <Menu
-                                    navigateToSettings={this.props.navigateToSettings}
-                                    navigateToDashboard={this.props.navigateToDashboard} />
-                            </div>
-                        </div>
-                        <div className="shaded">
-                            <Control socket={this.state.socket} />
-                        </div>
-                    </div >
-                );
-            }
-        } else {
-            return (
-                <div className="container">
-                    <div className="bufferContainer">
-                        <div id="menuContainerId" className="menuContainer">
-                            <Menu
-                                navigateToSettings={this.props.navigateToSettings}
-                                navigateToDashboard={this.props.navigateToDashboard} />
-                        </div>
-                    </div>
-                    <div>You are unauthorized user</div>
-                </div>
-            );
-        }
+		this.setState({
+			connected: true,
+			socket: socket
+		});
+		this.props.loadingStop();
+	}
 
-    }
+	disconnect() {
+		this.props.loading();
+		this.setState({
+			connected: false,
+			socket: null
+		});
+		this.props.loadingStop();
+	}
+
+	render() {
+		if (this.props.isAuthorized) {
+			if (this.state.connected) {
+				return (
+					<div className="container">
+						<div className="bufferContainer">
+							<ConnectionStatus
+								connectionState={this.state.connected}
+								connect={this.connect}
+								disconnect={this.disconnect}
+							/>
+							<div id="menuContainerId" className="menuContainer">
+								<Menu
+									navigateToSettings={this.props.navigateToSettings}
+									navigateToDashboard={this.props.navigateToDashboard}
+								/>
+							</div>
+						</div>
+						<Control socket={this.state.socket} />
+					</div>
+				);
+			} else {
+				return (
+					<div className="container">
+						<div className="bufferContainer">
+							<ConnectionStatus
+								connectionState={this.state.connected}
+								connect={this.connect}
+								disconnect={this.disconnect}
+							/>
+							<div id="menuContainerId" className="menuContainer">
+								<Menu
+									navigateToSettings={this.props.navigateToSettings}
+									navigateToDashboard={this.props.navigateToDashboard}
+								/>
+							</div>
+						</div>
+						<div className="shaded">
+							<Control socket={this.state.socket} />
+						</div>
+					</div>
+				);
+			}
+		} else {
+			return (
+				<div className="container">
+					<div className="bufferContainer">
+						<div id="menuContainerId" className="menuContainer">
+							<Menu
+								navigateToSettings={this.props.navigateToSettings}
+								navigateToDashboard={this.props.navigateToDashboard}
+							/>
+						</div>
+					</div>
+					<div>You are unauthorized user</div>
+				</div>
+			);
+		}
+	}
 }
 
 export default Dashboard;
