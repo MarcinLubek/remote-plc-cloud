3cc507203666342f6f9b3d2e9da33d011d1bbf32
Marcin Łubek <m.lubek@polsource.com>
2019-07-21 19:46:49 +0200

    faza

--- src/ConnectionStatus.js
+++ src/ConnectionStatus.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react";
 
 function ConnectionStatus(props) {
+	function toggleConnection(e) {
+		if (props.connectionState) {
+			props.disconnect();
+		} else {
+			props.connect();
+		}
+	}
 
-    function toggleConnection(e) {
-        if (props.connectionState) {
-            props.disconnect();
-        } else {
-            props.connect();
-        }
-    }
-
-    if (props.connectionState) {
-        return (
-            <div className="connectionStatus" onClick={toggleConnection}>
-                <div className="led green"></div>
-                <div>Connected</div>
-            </div >
-        );
-    } else {
-        return (
-            <div className="connectionStatus" onClick={toggleConnection}>
-                <div className="led red"></div>
-                <div>Connect</div>
-            </div >
-        );
-    }
+	if (props.connectionState) {
+		return (
+			<div className="connectionStatus" onClick={toggleConnection}>
+				<div className="led green" />
+				<div>Connected</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="connectionStatus" onClick={toggleConnection}>
+				<div className="led red" />
+				<div>Connect</div>
+			</div>
+		);
+	}
 }
 
 export default ConnectionStatus;
